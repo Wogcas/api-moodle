48bc74bbb7c0f323aa071ec6a8ebcde47738be4b
Wogcas <gael.castro247887@potros.itson.edu.mx>
2025-04-01 19:41:41 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# api-moodle
+ This is just a mock up for now with academic and learning purposes.
---
+++ src/main/java/server/rest/api_moodle/ApiMoodleApplication.java
@@ -0,0 +1,13 @@
+package server.rest.api_moodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMoodleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMoodleApplication.class, args);
+	}
+
+}
